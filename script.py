e9dc81eabd7c086415a780190b14a7ab458fd010
GopinathSM17 <58787612+GopinathSM17@users.noreply.github.com>
2025-04-16 15:05:53 +0530

    basics of python

---
+++ script.py
@@ -0,0 +1,16 @@
+a = ""
+b= 1
+c=2
+
+print("This is gopinath file")
+
+friends = ['John','Michael','Terry','Eric','Graham']
+print(friends)
+
+name = input('what is your name:  ')
+print(f"this is my {name}")
+ 
+def grreeting(name, age, company):
+    print(f'hello {name}')
+
+grreeting("gopinath", 20, "Mountblue technologies")
